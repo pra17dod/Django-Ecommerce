77e1fdcf663a38a37a5682609e26ee4822bdb3d5
pra17dod <pra17dod@gmail.com>
2021-05-01 08:48:23 +0530

    views.py: Add products, checkout and home views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+
+def products(request):
+    ctx = {'items': Item.objects.all()}
+
+    return render(request, "products.html", ctx)
+
+
+def checkout(request):
+    return render(request, "checkout.html")
+
+
+def home(request):
+    ctx = {'items': Item.objects.all()}
+    return render(request, 'home.html', ctx)
